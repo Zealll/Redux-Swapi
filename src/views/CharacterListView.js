3be62f9c5c0d648b2b109600bcc92d585ce55ebd
unknown <eriznis@gmail.com>
2019-03-07 08:48:40 -0500

    getting some error and still wasnt able to fix it

--- src/views/CharacterListView.js
+++ src/views/CharacterListView.js
@@ -3,19 +3,25 @@ import { connect } from "react-redux";
 
 import { CharacterList } from "../components";
 // import actions
+import {getCharacters} from '../actions'
 
 class CharacterListView extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
   }
 
   componentDidMount() {
     // call our action
+    this.props.getCharacters()
   }
 
   render() {
     if (this.props.fetching) {
       // return something here to indicate that you are fetching data
+      return <h1>Character's taking a bathroom break...</h1>
+    }
+    if (this.props.error) {
+      return <h1>{this.props.error}</h1>
     }
     return (
       <div className="CharactersList_wrapper">
@@ -27,9 +33,17 @@ class CharacterListView extends React.Component {
 
 // our mapStateToProps needs to have two properties inherited from state
 // the characters and the fetching boolean
+const mapStateToProps = state => {
+  return {
+    characters: state.charsReducer.characters,
+    error: state.charsReducer.error,
+    fetching: state.charsReducer.fetching
+  }
+}
 export default connect(
-  null /* mapStateToProps replaces null here */,
+  mapStateToProps /* mapStateToProps replaces null here */,
   {
     /* action creators go here */
+    getCharacters
   }
 )(CharacterListView);
